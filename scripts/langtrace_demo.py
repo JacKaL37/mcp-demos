0edaa0ae41cbc680f09b1323f242a33304e1ff25
John K. Lindstedt <john.k.lindstedt@gmail.com>
2025-06-02 12:34:50 -0400

    x

--- scripts/langtrace_demo.py
+++ scripts/langtrace_demo.py
@@ -2,11 +2,13 @@ from langtrace_python_sdk import langtrace
 from langtrace_python_sdk.utils.with_root_span import with_langtrace_root_span
 # Paste this code after your langtrace init function
 
+langtrace.init(
+    api_key="91e4463648736881178851e555131b0776b03d79b6d4fe8e4eef9e5a6363f49d",
+)
+
 from openai import OpenAI
 
-langtrace.init(
-    api_key="91e4463648736881178851e555131b0776b03d79b6d4fe8e4eef9e5a6363f49d"
-)
+
 
 @with_langtrace_root_span()
 def example():
